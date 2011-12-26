09f85b6badbd8f25f4adec75b74eb83993d4c0c9
marsz <marsz330@gmail.com>
2011-12-26 17:01:39 +0800

    nested attributes

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,7 @@
 class CitiesController < ApplicationController
   def index
-    @cities = City.includes(:name_aliases).enabled if !@cities
-    respond_as_api @cities, :include => :name_aliases
+    includes = [:name_aliases,{:dists=>[:name_aliases]}]
+    @cities = City.includes(includes).enabled if !@cities
+    respond_as_api @cities, :include => {:name_aliases=>{:only=>:name}, :dists=>{:only=>:name,:include=>{:name_aliases=>{:only=>:name}}}}
   end
 end
