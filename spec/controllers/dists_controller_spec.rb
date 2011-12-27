20ffd24a32196e13f57a3fcbbc7f3619e59a6cc8
marsz <marsz330@gmail.com>
2011-12-28 00:53:59 +0800

    specs of api interface

--- spec/controllers/dists_controller_spec.rb
+++ spec/controllers/dists_controller_spec.rb
@@ -14,33 +14,19 @@ describe DistsController do
     
     before do
       @city = Factory :city
-      @dist_enabled = Factory :dist_enabled, :city_id => @city.id
+      @dist = @dist_enabled = Factory(:dist_enabled, :city_id => @city.id)
       @dist_name_alias_enabled = Factory :dist_name_alias_enabled, :dist_id => @dist_enabled.id
       @dist_name_alias_disabled = Factory :dist_name_alias_disabled, :dist_id => @dist_enabled.id
       @dist_disabled = Factory :dist_disabled, :city_id => @city.id
-      
     end
     
-    pending "move to share example"
-    
     describe "get show" do
-      before do
-        get :show, :city_id => @city.id, :id => @dist_enabled.id, :format => :json
-        @data = init_api_data
-      end
-      
-      it { @status.should == 200 }
-      it { @data.should be_a_kind_of(Hash) }
-      
-      it { @data[:id].to_i.should == @dist_enabled.id }
-      it { @data[:city_id].to_i.should == @city.id }
-      it { @data[:city][:name].should == @city.name }
-      it { @data[:city][:name_aliases].should be_a_kind_of(Array) }
-      it { @data[:name_aliases].should be_a_kind_of(Array) }
-      it "name aliases enabled only" do
-        alias_ids = @data[:name_aliases].map{ |hash| hash[:name] }
-        alias_ids.include?(@dist_name_alias_enabled.name).should be_true
-        alias_ids.include?(@dist_name_alias_disabled.name).should be_false
+      describe "enabled dist" do
+        before do
+          get :show, :city_id => @city.id, :id => @dist.id, :format => :json
+          @dist_hash = init_api_data
+        end
+        it_should_behave_like "api_data_dist"
       end
       
       it "disabled dist" do
@@ -53,30 +39,17 @@ describe DistsController do
     describe "get index" do
       before do
         get :index, :city_id => @city.id, :format => :json
-        @data = init_api_data
+        @dists = init_api_data
       end
       it { @status.should == 200 }
-      it { @data.should be_a_kind_of(Array) }
       describe "data format validate" do
-        before do
-          @dist_hash = @data.first
+        describe "dist" do
+          before do
+            @dist_hash = @dists.first
+          end
+          it_should_behave_like "api_data_dist"
         end
-        it { @dist_hash[:id].to_i.should == @dist_enabled.id }
-        it { @data.each { |dist_hash| dist_hash[:city_id].to_i.should == @city.id } }
-        it { @dist_hash[:city][:name].should == @city.name }
-        it { @dist_hash[:city][:name_aliases].should be_a_kind_of(Array) }
-        it { @data.should be_a_kind_of(Array) }
-        it { @dist_hash[:name_aliases].should be_a_kind_of(Array) }
-        it "name aliases enabled only" do
-          alias_ids = @dist_hash[:name_aliases].map{ |hash| hash[:name] }
-          alias_ids.include?(@dist_name_alias_enabled.name).should be_true
-          alias_ids.include?(@dist_name_alias_disabled.name).should be_false
-        end
-        it "only enabled dists" do
-          dist_ids = @data.map{ |h| h[:id].to_i }
-          dist_ids.include?(@dist_enabled.id).should be_true
-          dist_ids.include?(@dist_disabled.id).should be_false
-        end
+        it_should_behave_like "api_data_dists"
       end
     end
     
