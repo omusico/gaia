136bf63296decc824d14e8bbf17ef13760dfb198
marsz <marsz330@gmail.com>
2012-12-22 13:44:40 +0800

    selector js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+# encoding: utf-8
 module ApplicationHelper
+  def render_select(collection, selected, html_options = {})
+    html_options[:include_blank] = false
+    html_options[:prompt] = "請選擇"
+    select_tag html_options[:name], options_from_collection_for_select(collection, :id, :name, selected), html_options
+  end
 end
